673925604ce4278030fd6b373bae2c520f6c94d5
piao <prz_2022@qq.com>
2026-04-12 17:16:09 +0800

    新增认证服务

---
+++ src/main/java/com/example/demo/identity/IdentityService.java
@@ -0,0 +1,35 @@
+package com.example.demo.identity;
+
+import org.openstack4j.api.OSClient;
+import org.openstack4j.model.common.Identifier;
+import org.openstack4j.openstack.OSFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdentityService {
+
+    @Value("${openstack.endpoint}")
+    private String endpoint;
+
+    @Value("${openstack.userId}")
+    private String userId;
+
+    @Value("${openstack.secret}")
+    private String secret;
+
+    @Value("${openstack.projectName}")
+    private String projectName;
+
+    @Value("${openstack.domainName}")
+    private String domainName;
+
+    public OSClient.OSClientV3 getOSClientV3() {
+        OSClient.OSClientV3 os = OSFactory.builderV3()
+                .endpoint(endpoint)
+                .credentials(userId, secret, Identifier.byName(domainName))
+                .scopeToProject(Identifier.byName(projectName), Identifier.byName(domainName))
+                .authenticate();
+        return os;
+    }
+}
